b1e032e18d11d104f02457366820884726a76b50
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-20 23:12:07 +0100

    build: create database and models

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ const bodyParser = require("koa-parser");
 const app = new Koa();
 const port = 4000;
 
+const db = require("./models");
+db.sequelize
+  .sync()
+  .then(() => console.log("models synced!"))
+  .catch((err) => console.log(err));
+
 app.listen(port);
 console.log(`Server is listenng on port ${port}`);
