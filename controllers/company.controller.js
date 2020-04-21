b24be45d9754d187f316a899934879ed3d8fd863
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-21 01:02:27 +0100

    build: create findOne endpoint

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -2,11 +2,31 @@ module.exports = {
   // create a new company record
   async create(ctx) {
     try {
-      ctx.body = await ctx.db.Company.create({
-        name: ctx.request.body.name,
-        city: ctx.request.body.city,
-        address: ctx.request.body.address,
-      });
+      let { name, city, address } = ctx.request.body;
+
+      switch (undefined) {
+        case name:
+          ctx.throw(400, "name is required");
+        case city:
+          ctx.throw(400, "city is required");
+        case address:
+          ctx.throw(400, "address is required");
+          break;
+
+        default:
+          ctx.body = await ctx.db.Company.create({
+            name: ctx.request.body.name,
+            city: ctx.request.body.city,
+            address: ctx.request.body.address,
+          });
+          break;
+      }
+
+      //   ctx.body = await ctx.db.Company.create({
+      //     name: ctx.request.body.name,
+      //     city: ctx.request.body.city,
+      //     address: ctx.request.body.address,
+      //   });
     } catch (error) {
       ctx.throw(500, error);
     }
@@ -15,6 +35,19 @@ module.exports = {
   async find(ctx) {
     try {
       ctx.body = await ctx.db.Company.findAll();
+    } catch (error) {
+      ctx.throw(500, error);
+    }
+  },
+  // find one company record
+  async findOne(ctx) {
+    try {
+      const company = await ctx.db.Company.findOne({
+        where: {
+          id: ctx.params.id,
+        },
+      });
+      !company ? ctx.throw(404, "company id not found") : (ctx.body = company);
     } catch (error) {
       ctx.throw(500, error);
     }
