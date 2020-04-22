dca9456c623770b48e9c53ba9ca2da144d125e0f
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-22 16:15:18 +0100

    build: install koa-static
    for serving static files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-parser");
 const _ = require("lodash");
+const serve = require("koa-static");
 
 const router = require("./routes");
 
@@ -17,6 +18,7 @@ db.sequelize
 app.context.db = db;
 app.use(bodyParser());
 app.use(router.routes());
+app.use(serve(__dirname + "/public"));
 
 app.listen(port);
 console.log(`Server is listenng on port ${port}`);
